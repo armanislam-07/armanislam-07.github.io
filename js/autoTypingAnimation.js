9b4c4f426b600c0a27051c4dfabfec6c22223312
Arman Islam <armanislamusa@gmail.com>
2025-11-26 12:16:40 -0500

    refactor: overhaul project by replacing previous template with new one

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Arman Islam</span> I'm an electrical engineer studying at Purdue University passionate about Embedded Systems, Computer Vision, Robotics, and Cybersecurity. I love turning ideas you see in sci-fi movies into real life. Follow my github and LinkedIn to see these projects emerge to real life!"
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
