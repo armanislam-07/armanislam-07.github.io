704c9e44d5a111337fbdfb152e63781312e4c30c
Arman Islam <armanislamusa@gmail.com>
2026-04-01 10:48:09 -0400

    Removed CV button and fixed spelling in about me

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,7 +1,7 @@
 setTimeout(function() {
     $('.about-info-inner').typed({
       strings: [
-        "<span>Hi, I am Arman Islam</span> I'm an electrical engineer studying at Purdue University passionate about Embedded Systems, Research, and anything engineering. I love turning ideas you see in movies into real life. Follow my github and LinkedIn to see these projects emerge to real life!"
+        "<span>Hi, I am Arman Islam</span> I'm an electrical engineer studying at Purdue University passionate about Embedded Systems, Research, and anything engineering. I love turning ideas you see in movies into real life. Follow my Github and LinkedIn to see these projects emerge to real life!"
       ],
       typeSpeed: 7,
       contentType: 'html'
